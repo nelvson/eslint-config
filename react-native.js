81e3b9e23bc50bb775df41421f56d88dade4ebea
nelvson <nelvson.shine@warungpintar.co>
2022-04-17 12:03:11 +0700

    init

---
+++ react-native.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'warung-config/react',
+  env: {
+    'react-native/react-native': true,
+  },
+  plugins: ['react-native'],
+  rules: {
+    // React Native (eslint-plugin-react-native)
+    'react-native/no-unused-styles': 'warn',
+    'react-native/no-single-element-style-arrays': 'warn',
+  },
+};
